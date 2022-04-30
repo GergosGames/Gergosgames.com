866116747787470261074b947d56e8f747f90916
GergosGames <gergo.kaszas@outlook.com>
2022-05-01 07:16:09 +1000

    a

---
+++ !Test/index.php
@@ -0,0 +1,30 @@
+<div>
+      
+
+      <h1 className="text-3xl font-bold text-gray">
+        Gray       IIII
+      </h1>
+
+      <h1 className="text-3xl font-bold text-dark-gray">
+        Dark gray  IIII
+      </h1>
+
+      <h1 className="text-3xl font-bold text-light-gray">
+        Light gray IIII
+      </h1>
+
+      <h1 className="text-3xl font-bold text-blue1">
+        Blue1      IIII
+      </h1>
+
+      <h1 className="text-3xl font-bold text-blue2">
+        Blue2      IIII
+      </h1>
+
+      <h1 className="text-3xl font-bold text-dark-blue">
+        Dark Blue  IIII
+      </h1>
+    </div>
+
+    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
